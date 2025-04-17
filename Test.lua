766b777605d7ce5880fc480667196dc5f4afad65
RebornPhantom <83071483+RebornPhantom@users.noreply.github.com>
2025-04-17 18:00:03 -0400

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,16 +1,3 @@
-local gui = Instance.new("ScreenGui")
-gui.Name = "InjectedGui"
-gui.Parent = Players.LocalPlayer:WaitForChild("PlayerGui")
-
-local button = Instance.new("TextButton")
-button.Text = "Click Me"
-button.Size = UDim2.new(0, 200, 0, 50)
-button.Position = UDim2.new(0.5, -100, 0.5, -25)
-button.BackgroundColor3 = Color3.fromRGB(0, 170, 255)
-button.Parent = gui
-
-function click()
-	print("Live code worked!")
-end
-
-button.MouseButton1Click:Connect(click)
+local scriptInstance = Instance.new("Script")
+scriptInstance.Name = "TestLoad"
+scriptInstance.Parent = ReplicatedStorage
