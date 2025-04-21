f99368b5647d9ccf6483413db4e80b7e7d8f0bfa
RebornPhantom <83071483+RebornPhantom@users.noreply.github.com>
2025-04-20 20:31:26 -0400

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,7 +1,11 @@
-function Main()
-	local count = 0
-	while count < 3 do
-		print("LoopIteration:", count)
-		count = count + 1
+local count = 0
+while count < 5 do
+	count = count + 1
+	if count == 2 then
+		continue
+	elseif count == 4 then
+		break
+	else
+		print("Count:", count)
 	end
 end
