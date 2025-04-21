cd975272ba17f8e7f0f1c33ed3e63f021093226c
RebornPhantom <83071483+RebornPhantom@users.noreply.github.com>
2025-04-20 20:54:46 -0400

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -4,16 +4,19 @@ function Main()
 
 	local part = Instance.new("Part")
 	part.Size = Vector3.new(2, 1, 2)
-	part.Position = player.Character and player.Character:GetPivot().Position or Vector3.new(0, 5, 0)
 	part.Anchored = true
 	part.BrickColor = BrickColor.new("Bright red")
 	part.Name = "TestBlock"
 
 	if player.Character then
+		part.Position = player.Character:GetPivot().Position
 		part.Parent = player.Character
 		print("Part Parent:", part.Parent)
 		print("Part Position:", part.Position)
 	else
+		part.Position = Vector3.new(0, 5, 0)
+		part.Parent = workspace
 		print("Character not found.")
+		print("Part placed in workspace at:", part.Position)
 	end
 end
