c403b427c42f2fcaa712f4d6bbe4de2e3e81b148
RebornPhantom <83071483+RebornPhantom@users.noreply.github.com>
2025-04-21 20:06:26 -0400

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -7,16 +7,15 @@ function Main()
 	part.BrickColor = BrickColor.new("Bright red")
 	part.Name = "TestBlock"
 
-	print("Character value:", game.Players.LocalPlayer.Character)
 	if game.Players.LocalPlayer.Character then
 		part.Position = game.Players.LocalPlayer.Character:GetPivot().Position
 		part.Parent = game.Players.LocalPlayer.Character
-		print("Part Parent:", part.Parent)
-		print("Part Position:", part.Position)
 	else
 		part.Position = Vector3.new(0, 5, 0)
 		part.Parent = workspace
-		print("Character not found.")
-		print("Part placed in workspace at:", part.Position)
 	end
+
+	print("PartParent:", part.Parent)
+	print("PartPosition:", part.Position)
+	print("Partplacedinworkspacet:", part.Position)
 end
