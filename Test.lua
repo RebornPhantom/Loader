edc127e73a7ef05fe6c3cc1b259a65d3927a4d2e
RebornPhantom <83071483+RebornPhantom@users.noreply.github.com>
2025-04-21 20:20:06 -0400

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,5 +1,8 @@
 function Main()
-	print("LocalPlayer:", game.Players.LocalPlayer.Name)
+	local lp = LocalPlayer
+	print("LocalPlayer:", lp.Name)
+
+	repeat task.wait() until lp.Character
 
 	local part = Instance.new("Part")
 	part.Size = Vector3.new(2, 1, 2)
@@ -7,15 +10,10 @@ function Main()
 	part.BrickColor = BrickColor.new("Bright red")
 	part.Name = "TestBlock"
 
-	if game.Players.LocalPlayer.Character then
-		part.Position = game.Players.LocalPlayer.Character:GetPivot().Position
-		part.Parent = game.Players.LocalPlayer.Character
-	else
-		part.Position = Vector3.new(0, 5, 0)
-		part.Parent = workspace
-	end
+	local char = lp.Character
+	part.Position = char:GetPivot().Position
+	part.Parent = char
 
-	print("PartParent:", part.Parent)
 	print("PartPosition:", part.Position)
 	print("Partplacedinworkspacet:", part.Position)
 end
