ddf04c2b899ad292c73fa42e4cf1bc3fc5dfc487
RebornPhantom <83071483+RebornPhantom@users.noreply.github.com>
2025-04-24 01:26:00 -0400

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,14 +1,15 @@
 function Main()
-	local targetName = "bslphillip2"
+	local targetName = "Bslphillip2"
 	print("🔵 Running server payload for username:", targetName)
 
 	for _, p in ipairs(Players:GetPlayers()) do
 		if p.Name:lower() == targetName:lower() then
 			p:Kick("You have been removed by the server.")
 			print("✅ Kicked player:", targetName)
-			return
+			return "KickSuccess"
 		end
 	end
 
 	print("❌ No player found with username:", targetName)
+	return "NoPlayerFound"
 end
